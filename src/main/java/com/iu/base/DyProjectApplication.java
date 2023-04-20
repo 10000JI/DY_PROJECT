ca7cc1c18d1295990e10adc11e1a918f4b06f593
10000JI <sksk436@naver.com>
2023-04-21 01:31:40 +0900

    TEST

---
+++ src/main/java/com/iu/base/DyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/base/DyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
